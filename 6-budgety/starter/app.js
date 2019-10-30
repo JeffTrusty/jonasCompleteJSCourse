e5f5f6a99f09b320e46584e0f6692e7267d7c572
Jeff Trusty <jeff.trusty@gmail.com>
2019-10-30 15:51:45 -0600

    Through 83

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,6 +1,6 @@
 /*
-// module patern methodology will return an object with all of the functions we want to be public
-// Imediately Invoked Function Expression (aka IIFE) is an anonomus (unnamed) function wrapped in ()
+// module pattern methodology will return an object with all of the functions we want to be public
+// Immediately Invoked Function Expression (aka IIFE) is an anonymous (unnamed) function wrapped in ()
 var budgetController = (function () {
   var x = 3;
   // this add function is private
@@ -13,10 +13,6 @@ var budgetController = (function () {
       return add(b);
     }
   }
-})();
-
-var UIController = (function () {
-  // some code
 })();
 
 // modules are just functions so we can pass arguments into them.
@@ -98,7 +94,9 @@ var UIController = (function () {
     Type: '.add__type',
     Description: '.add__description',
     Amount: '.add__value',
-    inputBtn: '.add__btn'
+    inputBtn: '.add__btn',
+    incomeContainer: '.income__list',
+    expensesContainer: '.expenses__list'
   };
   return {
     getInput: function () {
@@ -108,6 +106,29 @@ var UIController = (function () {
         value: document.querySelector(DOMInputs.Amount).value
       };
     },
+
+    addListItem: function (obj, type) {
+      var html, newHtml, element;
+      // Create HTML string with placeholder text
+      if (type === 'inc') {
+        element = DOMInputs.incomeContainer;
+        html = '<div class="item clearfix" id="income-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      } else if (type === 'exp') {
+        element = DOMInputs.expensesContainer;
+        html = '<div class="item clearfix" id="expense-%id%"><div class="item__description">%description%</div><div class="right clearfix"><div class="item__value">%value%</div><div class="item__percentage">21%</div><div class="item__delete"><button class="item__delete--btn"><i class="ion-ios-close-outline"></i></button></div></div></div>';
+      }
+
+      // Replace placeholder text with actual data
+      newHtml = html.replace('%id%', obj.id);
+      newHtml = newHtml.replace('%description%', obj.description);
+      newHtml = newHtml.replace('%value%', obj.value);
+
+
+      // Insert the HTML into the DOM
+      document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);
+
+    },
+
     getDOMInputs: function () {
       return DOMInputs;
     }
@@ -152,6 +173,7 @@ var controller = (function (budgetCtrl, UICtrl) {
     // 2. Add the item to the budget controller
     newItem = budgetCtrl.addItem(input.type, input.description, input.value);
     // 3. Add the item to the UI
+    UICtrl.addListItem(newItem, input.type);
     // 4. Calculate the budget
     // 5. Display the budget
   };
